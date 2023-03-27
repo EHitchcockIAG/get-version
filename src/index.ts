c82647bf818f881823cc6c53f9f5f4289528d304
Ethan Hitchcock <ethan.hitchcock@iag.co.nz>
2023-03-27 15:22:14 +1300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import { parse } from 'semver';
+import { resolve } from 'path';
+import * as logging from './helpers/logging';
+
+function getGithubInput () {
+    return {
+        packagePath: core.getInput('package') || 'package.json'
+    }
+}
+
+async function main () {
+    const { packagePath } = getGithubInput();
+    logging.info('Using ', packagePath, ' as package directory');
+
+
+    try {
+        const pkg = resolve(packagePath);
+        const { version } = require(pkg);
+        const parsedVersion = parse(version);
+
+        core.setOutput('version', parsedVersion?.version);
+        core.setOutput('version-major', parsedVersion?.major);
+        core.setOutput('version-minor', parsedVersion?.minor);
+        core.setOutput('version-patch', parsedVersion?.patch);
+    } catch (e: any) {
+        logging.error("Unable to parse version from ", packagePath);
+        throw Error(e);
+    }
+}
+
+main().catch(e => {
+    core.setFailed(`get-version failed: ${e.message}`)
+})
